fb0e90142e4f90474dd2425676de2c2aa89452ac
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2021-05-15 18:38:49 -0500

    Fix solution

--- 2021/RoundB/A-IncreasingSubstring.cpp
+++ 2021/RoundB/A-IncreasingSubstring.cpp
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <vector>
+#include <iostream>
+#include <string>
 using namespace std;
 
 // round 709
 
 int main(){
 
-    int t; 
-    scanf("%d", &t);
-    while(t--){
-        int a, b;
-        scanf("%d %d", &a, &b);
-        printf("%d\n", a * b);
+    int ts;
+    cin >> ts;
+    for(int t = 0; t < ts; t++){
+        cout << "Case #" << t + 1 << ":";
+        int n;
+        string s;
+        cin >> n >> s;
+        vector<int>ans = vector<int> (n);
+        ans[0] = 1;
+        int acc = 1;
+        cout << " " << acc;
+        for(int i = 1; i < n; i++){
+            if(s[i] > s[i - 1]){
+                acc++;
+                ans[i] = acc;
+                cout << " " << acc;
+            }else{
+                acc = 1;
+                cout << " 1";
+            }
+        }
+        cout << endl;
+    
     }
     return 0;
 }
